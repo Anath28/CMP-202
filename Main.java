a4c4a6e119a86d8d7c6b5a7e67a2b419b173f905
Kyenshak Bulus <132848743+Anath28@users.noreply.github.com>
2024-05-28 13:57:06 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 
 public class Main {
 
+
+
+    
+
+
+
     public static void main(String[] args) {
-        cmp202 cmp202 = new cmp202();
+        Computer hp250 = new Computer(new RAM(), new Processor(), new Storage());
+        System.out.println(hp250);
+
+
+
+    /*cmp202 cmp202 = new cmp202();
         cmp202.printUserDetails();
 
         Person student = new Person("Bingham", "Anath", "12345", "0107");
@@ -12,8 +23,8 @@ public class Main {
         System.out.println(calculate.calculateVelocity(50, 10));
         System.out.println(calculate.calculateVelocity(7, 15, 2));
         System.out.println(student);
-        //System.out.println(Person.getName());
+        //System.out.println(Person.getName());*/
 
 
     }
-}
+    }
